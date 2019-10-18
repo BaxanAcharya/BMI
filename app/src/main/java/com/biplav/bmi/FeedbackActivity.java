fe8d95714a84bff92f128418b260a373df9c9e6c
BaxanAcharya <baxanacharya@gmail.com>
2019-10-18 08:53:30 +0545

    Giving feedback

--- app/src/main/java/com/biplav/bmi/FeedbackActivity.java
+++ app/src/main/java/com/biplav/bmi/FeedbackActivity.java
@@ -14,10 +14,34 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         Bundle bundle=getIntent().getExtras();
-        String stuff=bundle.getString("ans");
+        float stuff=Float.parseFloat(bundle.getString("ans"));
 
         tvmyans=findViewById(R.id.tvfinal);
-        tvmyans.setText("You EMI is: "+stuff);
+        //tvmyans.setText("You EMI is: "+stuff);
+        if (stuff<=16)
+        {
+            tvmyans.setText("Severe thickness");
+        }
+        else if(stuff>16 && stuff<=17)
+        {
+            tvmyans.setText("Moderate thickness");
+        }
+        else if(stuff>17 && stuff<=18.5)
+        {
+            tvmyans.setText("Mild thickness");
+        }
+        else if(stuff>18.5 && stuff<=25)
+        {
+            tvmyans.setText("Normal");
+        }
+        else if(stuff>25 && stuff<=30)
+        {
+            tvmyans.setText("Overweight");
+        }
+        else if(stuff>30)
+        {
+            tvmyans.setText("Obese class");
+        }
 
     }
 }
